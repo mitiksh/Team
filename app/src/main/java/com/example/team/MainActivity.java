123afd2fac302a518d59009b1123b0bfd6baed04
mitiksh <mitixsuthar.ms@gmail.com>
2020-11-18 13:28:26 +0530

    update

--- app/src/main/java/com/example/team/MainActivity.java
+++ app/src/main/java/com/example/team/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.team;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.RequestQueue;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private TextInputEditText username, password;
+    private RequestQueue requestQueue;
+    private Button loginButton;
+    private String emailHolder, passwordHolder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (TextInputEditText) findViewById(R.id.username);
+        password= (TextInputEditText) findViewById(R.id.password);
+
     }
+
+
 }
