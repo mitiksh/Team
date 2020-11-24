d4813cf8718ed1c29b5efda3524a3d9da399f71c
mitiksh <mitixsuthar.ms@gmail.com>
2020-11-24 21:15:48 +0530

    login and register done.
    do everthing experimental things in other repo

--- app/src/main/java/com/example/team/MainActivity.java
+++ app/src/main/java/com/example/team/MainActivity.java
@@ -2,20 +2,40 @@ package com.example.team;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-public class MainActivity extends AppCompatActivity {
-    private TextInputEditText username, password;
-    private RequestQueue requestQueue;
-    private Button loginButton;
-    private String emailHolder, passwordHolder;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity{
+    private TextInputEditText username, password;
+    private Button loginButton;
+    private String str_name, str_emal,str_password;
+    private TextView signup_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +44,56 @@ public class MainActivity extends AppCompatActivity {
 
         username = (TextInputEditText) findViewById(R.id.username);
         password= (TextInputEditText) findViewById(R.id.password);
+        signup_text = (TextView) findViewById(R.id.signUpText);
+        loginButton = (Button) findViewById(R.id.buttonLogin);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
 
     }
 
 
+    public void Login(){
+            StringRequest request = new StringRequest(Request.Method.POST, constants.loginUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if (jsonObject.getString("error")=="false"){
+                                Intent intent = new Intent(getApplicationContext(),home.class);
+                                startActivity(intent);
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username.getText().toString().trim());
+                params.put("password",password.getText().toString().trim());
+
+                return params;
+            }
+        };
+        Volley.newRequestQueue(this).add(request);
+    }
+
+    public void signup(View view) {
+        Intent intent = new Intent(this,signup.class);
+        startActivity(intent);
+    }
 }
